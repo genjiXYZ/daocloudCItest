264bb76bddc3ae3e8dc526d7cf981e568350293c
lly <35106038+genjiXYZ@users.noreply.github.com>
2020-06-13 06:21:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('hey baby')
+  res.send('hey guys')
 })
 app.get('/api', function (req, res) {
   res.send('api')
